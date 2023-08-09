03892e18be847b6ca850d8356e5ea42d9cb43dc0
mattebasta <matteobastante@gmail.com>
2023-08-09 15:06:41 +0200

    ...

--- tenutasantommasoweb/src/pages/restaurant.js
+++ tenutasantommasoweb/src/pages/restaurant.js
@@ -1,5 +1,8 @@
 import "./Restaurant.css";
 import ReturnBTN from "./component/upToPageButton.js";
+import barImg from "./img/MazzarellaCafe_after.png";
+import memImg from "./img/Memento_after.jpeg";
+import ondBluImg from "./img/ondaBluRestaurant_after.jpeg";
 
 const Restaurant = () => {
   return (
@@ -14,44 +17,54 @@ const Restaurant = () => {
         </p>
       </div>
       <div className="restContainer">
-        <div className="BarDiv">
-          <a
-            href="https://www.google.com/maps/dir/?api=1&origin=37.092317,15.161946&destination=37.0887772362438,15.162200803554175&travelmode=driving"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <p>Bar Mazzarella</p>
-          </a>
-          <p>
-            Perfect for breakfast, in summertime we reccomend the "Granita di
-            mandorla con brioche", <br />
-            which is a typical breakfast in sicily. <sub>**made with almond</sub>
-          </p>
-        </div>
-        <div className="mementoDiv">
-          <a
-            href="https://www.google.com/maps/dir/?api=1&origin=37.092317,15.161946&destination=37.081872,15.153473&travelmode=driving"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <p>Memento7</p>
-          </a>
-          <p>
-            If you want to enjoy a good pizza
-            <br />
-            this is the perfect place <sub>**opened only in the evening</sub>{" "}
-          </p>
-        </div>
-        <div className="ondaBluDiv">
-          <a
-            href="https://www.google.com/maps/dir/?api=1&origin=37.092317,15.161946&destination=37.03267194770378,15.284107615542435&travelmode=driving"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <p>Onda Blu</p>
-          </a>
-          <p>Good restaurant to eat typical fish dishes</p>
-        </div>
+        <a
+          href="https://www.google.com/maps/dir/?api=1&origin=37.092317,15.161946&destination=37.0887772362438,15.162200803554175&travelmode=driving"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="BarDiv">
+            <img src={barImg} alt="bar" />
+            <div>
+              <p>Bar Mazzarella</p>
+              <p>
+                Perfect for breakfast, in summertime we reccomend the "Granita
+                di mandorla con brioche" <br />
+                which is a typical breakfast in sicily <br />
+                <sub>**made with almond</sub>
+              </p>
+            </div>
+          </div>
+        </a>
+        <a
+          href="https://www.google.com/maps/dir/?api=1&origin=37.092317,15.161946&destination=37.081872,15.153473&travelmode=driving"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="mementoDiv">
+            <img src={memImg} alt="memento" />
+            <div>
+              <p>Memento7</p>
+              <p>
+                If you want to enjoy a good pizza
+                <br />
+                this is the perfect place <br />  <sub>**opened only in the evening</sub>
+              </p>
+            </div>
+          </div>
+        </a>
+        <a
+          href="https://www.google.com/maps/dir/?api=1&origin=37.092317,15.161946&destination=37.03267194770378,15.284107615542435&travelmode=driving"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="ondaBluDiv">
+            <img src={ondBluImg} alt="ondaBlu" />
+            <div>
+              <p>Onda Blu</p>
+              <p>Good restaurant to eat typical fish dishes</p>
+            </div>
+          </div>
+        </a>
       </div>
       <ReturnBTN></ReturnBTN>
     </div>
